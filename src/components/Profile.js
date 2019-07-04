68fe06d4af6952779cea2c8718d380eb7b75e9fc
evolvednight <edwinh13148@gmail.com>
2019-07-03 20:38:39 -0400

    change font for deployment test

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -17,7 +17,7 @@ class Profile extends Component {
                 
                 <div className="accessBtn">
                 <a href ="https://github.com/evolvednight"><Button><i class="fa fa-github-square"></i>GitHub</Button></a>
-                <a href="https://www.linkedin.com/in/ehun"><Button><i class="fa fa-linkedin-square"></i>LinkedIn</Button></a>
+                <a href = "https://www.linkedin.com/in/ehun"><Button><i class="fa fa-linkedin-square"></i>LinkedIn</Button></a>
                 <a href = {Pdf} target = "_blank"><Button><i class="far fa-address-card"></i>Resume</Button></a>
                 </div>
                 </Typing>
